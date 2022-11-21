b44525f7db95f2293b2717615091ac36d443e4ed
Amkanica <115214774+Amkanica@users.noreply.github.com>
2022-11-21 22:04:32 +0100

    change homepage text

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Demo App',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Ovo je moja Flutter aplikacija'),
+      home: const MyHomePage(title: 'Ovo je moja prva Flutter aplikacija'),
     );
   }
 }
