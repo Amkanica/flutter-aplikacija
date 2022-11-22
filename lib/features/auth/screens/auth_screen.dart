f9765f9a46128ac4f2f0f69e0465cb6324757499
Amkanica <115214774+Amkanica@users.noreply.github.com>
2022-11-22 23:54:39 +0100

    Custom Button for validation

--- lib/features/auth/screens/auth_screen.dart
+++ lib/features/auth/screens/auth_screen.dart
@@ -1,3 +1,4 @@
+import 'package:amazon_clone/common/widgets/custom_button.dart';
 import 'package:amazon_clone/common/widgets/custom_textfield.dart';
 import 'package:amazon_clone/constants/global_variables.dart';
 import 'package:flutter/material.dart';
@@ -40,6 +41,7 @@ class _AuthScreenState extends State<AuthScreen> {
         child: Padding(
           padding: const EdgeInsets.all(8.0),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               const Text(
                 'Welcome',
@@ -49,23 +51,26 @@ class _AuthScreenState extends State<AuthScreen> {
                 ),
               ),
               ListTile(
-                title: const Text(
-                  'Create Account',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                leading: Radio(
-                  activeColor: GlobalVariables.secondaryColor,
-                  value: Auth.signup,
-                  groupValue: _auth,
-                  onChanged: (Auth? val) {
-                    setState(() {
-                      _auth = val!;
-                    });
-                  },
-                ),
-              ),
+                    tileColor: _auth== Auth.signup ?
+                     GlobalVariables.backgroundColor:
+                     GlobalVariables.greyBackgroundCOlor,
+                    title: const Text(
+                           'Create Account',
+                    style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    leading: Radio(
+                    activeColor: GlobalVariables.secondaryColor,
+                    value: Auth.signup,
+                    groupValue: _auth,
+                    onChanged: (Auth? val) {
+                                 setState(() {
+                                _auth = val!;
+                               });
+                               },
+                      ),
+                      ),
               if (_auth == Auth.signup)
                 Container(
                   padding: const EdgeInsets.all(8),
@@ -90,17 +95,25 @@ class _AuthScreenState extends State<AuthScreen> {
                             ),
                         
                          const SizedBox(height: 10,),
-                        CustomTextField(
-                          controller:
-                           _passwordController,
-                           hintText: 'Password',
-                            ),
+                         CustomTextField(
+                            controller:
+                             _passwordController,
+                             hintText: 'Password',
+                             ),
 
-                            ],
+                            const SizedBox(height: 10,),
+                            CustomButton (
+                              text: 'Sign Up', 
+                              onTap: () {},
+                              ),
+                       ],
                     ),
                   ),
                 ),
               ListTile(
+                      tileColor: _auth== Auth.signin ?
+                     GlobalVariables.backgroundColor:
+                     GlobalVariables.greyBackgroundCOlor,
                 title: const Text(
                   'Sign-In.',
                   style: TextStyle(
@@ -118,6 +131,38 @@ class _AuthScreenState extends State<AuthScreen> {
                   },
                 ),
               ),
+        if (_auth == Auth.signin)
+                  Container(
+                  padding: const EdgeInsets.all(8),
+                  color: GlobalVariables.backgroundColor,
+                  child: Form(
+                    key: _signInFormKey,
+                    child: Column(
+                      children: [
+
+                        const SizedBox(height: 10,),
+                        CustomTextField(
+                           controller:
+                           _emailController,
+                           hintText: 'Email',
+                            ),
+                        
+                         const SizedBox(height: 10,),
+                         CustomTextField(
+                             controller:
+                             _passwordController,
+                             hintText: 'Password',
+                             ),
+
+                            const SizedBox(height: 10,),
+                            CustomButton (
+                              text: 'Sign In', 
+                              onTap: () {},
+                              ),
+                       ],
+                    ),
+                  ),
+                ),
             ],
           ),
         ),
